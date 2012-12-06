4c59736850b9105a2884d5ec3e2d843a11024be9
jumbojett <mikesjett@gmail.com>
2012-12-06 14:51:21 -0500

    Documentation updates

--- start.php
+++ start.php
@@ -1,19 +1,22 @@
 <?php
 
 /**
- * jettmail Plugin
+ *
  * @package jettmail
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
  * @author Michael Jett
  * @copyright MITRE
  * @link http://mitre.org/
+ *
+ * Approved for Public Release: 12-2907. Distribution Unlimited
+ *
  **/
 
 global $CONFIG;
 
 /**
- * let's do some requirements checking
- * the plugin will fail gracefully if not present
+ * Requirements checking begin
+ * The plugin will fail gracefully
  */
 if ((float)phpversion() < 5.3) {
     register_error("Jett mail requires at least php 5.3 or greater");
