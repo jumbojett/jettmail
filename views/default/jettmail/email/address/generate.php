3616245b8cd54959176b5dbd3129fd02f4325a6c
Jon Maul <jon@maul.com>
2014-05-09 10:51:08 -0400

    reply-by-email address no longer urlencoded (webmail clients balk at urlencodings in the email address portion of mailto links)

--- views/default/jettmail/email/address/generate.php
+++ views/default/jettmail/email/address/generate.php
@@ -3,6 +3,6 @@ $email_generator = new EmailAddressGenerator();
 $reply_email = $email_generator->generateEmailAddress($vars['action'] , $vars['guid'], $vars['to_email']);
 ?>
 
-<a href="mailto:<?= rawurlencode($reply_email) ?> ?subject=RE: <?= rawurlencode($vars['subject']) ?>">
+<a href="mailto:<?= $reply_email ?>?subject=RE:<?= rawurlencode($vars['subject']) ?>">
 <?= $vars['text']; ?>
 </a>
